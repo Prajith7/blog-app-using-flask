fce17996da3ca8f13c7e9e1ff1bf7ec41f53b81a
prajith <rprajith7@gmail.com>
2025-03-05 02:31:20 -0800

    env file bug

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
+from dotenv import load_dotenv
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
-
+load_dotenv()
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
@@ -28,9 +29,9 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+app.flash_key = os.environ.get('FLASH_KEY')
+app.config['SECRET_KEY'] = app.flash_key
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
